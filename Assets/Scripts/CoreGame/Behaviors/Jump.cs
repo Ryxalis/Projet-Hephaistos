ab6d2a0d3fd68aeb8471acf59fd886f87a6ac527
Ryxalis <ryl.ryx@laposte.net>
2018-04-27 20:04:03 +0000

    Adding ducking

--- Assets/Scripts/CoreGame/Behaviors/Jump.cs
+++ Assets/Scripts/CoreGame/Behaviors/Jump.cs
@@ -5,18 +5,34 @@ using UnityEngine;
 public class Jump : AbstractBehavior {
 
 	public float jumpSpeed = 200f;
+	public float jumpDelay = 0.1f;
+	public int jumpCount = 2;
+	public GameObject dustEffectPrefab;
+
+	protected float lastJumpTime = 0;
+	protected int jumpsRemaining = 0;
 
 	void Start () {
 		
 	}
 
-	void Update () {
-		var canJump = inputState.GetButtonValue (inputButtons [0]);
+	protected virtual void Update () {
+		var buttonJump = inputState.GetButtonValue (inputButtons [0]);
 		var buttonHoldTime = inputState.GetButtonHoldTime (inputButtons [0]);
 
 		if (collisionState.standing) {
-			if (canJump && buttonHoldTime < 0.1f) {
+			if (buttonJump && buttonHoldTime < 0.1f) {
+				jumpsRemaining = jumpCount - 1;
 				OnJump ();
+			}
+		} else {
+			if (buttonJump && buttonHoldTime < 0.1f && Time.time - lastJumpTime > jumpDelay) {
+				if (jumpsRemaining > 0) {
+					OnJump ();
+					jumpsRemaining--;
+					var clone = Instantiate (dustEffectPrefab);
+					clone.transform.position = transform.position;
+				}
 			}
 		}
 	}
@@ -25,5 +41,7 @@ public class Jump : AbstractBehavior {
 		var vel = body2D.velocity;
 
 		body2D.velocity = new Vector2 (vel.x, jumpSpeed);
+
+		lastJumpTime = Time.time;
 	}
 }
