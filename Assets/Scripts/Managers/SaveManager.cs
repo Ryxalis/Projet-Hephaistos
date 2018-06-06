d068fbc7a75746e3de6388e9bc413602500c9ac9
Ryxalis <ryl.ryx@laposte.net>
2018-06-06 16:45:18 +0000

    Save system began

--- Assets/Scripts/Managers/SaveManager.cs
+++ Assets/Scripts/Managers/SaveManager.cs
@@ -1,31 +1,58 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 
 public class SaveManager : MonoBehaviour {
 
 	public GameObject worldMap;
 
-	private int profilNumber = -1;
+	private int profileNumber = -1;
+	private int currentNode = -1;
 	private int[] nodeStatus;
 
 	public void SetProfilNumber(int newProfilNumber){
-		profilNumber = newProfilNumber;
+		profileNumber = newProfilNumber;
 	}
 
 	void Start(){
 		//Save ();
+		Load();
 	}
 
-	public void Save(){
+	/*public void Save(){
 		AbstractGameNode[] gameNodes = 	worldMap.GetComponentsInChildren<AbstractGameNode> ();
 		nodeStatus = new int[gameNodes.Length];
 		for(int i = 0; i<gameNodes.Length; ++i){
 			nodeStatus[i] = (int)gameNodes[i].nodeStatus;
 		}
+	}*/
+
+
+	public void Save(){
+		SaveData data = new SaveData ();
+		Stream stream = File.Open ("Save.game", FileMode.Create);
+		BinaryFormatter bformatter = new BinaryFormatter ();
+		bformatter.Binder = new VersionDeserializationBinder ();
+		Debug.Log ("Writing information");
+		bformatter.Serialize (stream, data);
+		stream.Close ();
 	}
 
 	public void Load(){
+		SaveData data = new SaveData ();
+		Stream stream = File.Open("Save.game", FileMode.Open);
+		BinaryFormatter bformatter = new BinaryFormatter();
+		bformatter.Binder = new VersionDeserializationBinder(); 
+		Debug.Log ("Reading Data");
+		data = (SaveData)bformatter.Deserialize(stream);
+		stream.Close();
 
+		currentNode = data.currentNode;
+		profileNumber = data.profileNumber;
+		for (int i = 0; i < data.nodeStatus.Length; ++i) {
+			nodeStatus [i] = data.nodeStatus [i];
+		}
 	}
 }
---
+++ Assets/Scripts/Save/SaveData.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.Serialization;
+
+[System.Serializable]
+public class SaveData : ISerializable {
+	public int[] nodeStatus = {0, 1};
+	public int currentNode = 42;
+	public int profileNumber = -1;
+
+	public SaveData(){
+	}
+
+	public SaveData(SerializationInfo info, StreamingContext ctxt){
+		for (int i = 0; i < nodeStatus.Length; ++i) {
+			nodeStatus[i] = (int)info.GetValue ("nodeStatus_" + i.ToString(), typeof(int));
+		}
+		currentNode = (int)info.GetValue ("currentNode", typeof(int));
+		profileNumber = (int)info.GetValue ("profileNumber", typeof(int));
+	}
+
+	public void GetObjectData(SerializationInfo info, StreamingContext ctxt){
+		for (int i = 0; i < nodeStatus.Length; ++i) {
+			info.AddValue ("nodeStatus_" + i.ToString(), nodeStatus[i]);
+		}
+		info.AddValue ("currentNode", currentNode);
+		info.AddValue ("profileNumber", profileNumber);
+	}
+}
